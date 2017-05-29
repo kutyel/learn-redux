89aca8b534850cf9a91cea05960f1b58bf211837
kutyel <flaviocorpa@gmail.com>
2017-05-29 08:31:38 +0200

    Add starter files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # learn-redux
+
+[![Standard - JavaScript Style Guide](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
+
 My solutions to the Learn Redux course
---
+++ client/data/config.js
@@ -0,0 +1,13 @@
+import Raven from 'raven-js'
+
+const sentryKey = 'cb55d4f05cd443ce82303222f77ef5e0'
+const sentryApp = '61499'
+export const sentryUrl = `https://${sentryKey}@app.getsentry.com/${sentryApp}`
+
+export function logException (ex, context) {
+  Raven.captureException(ex, {
+    extra: context
+  })
+  /* eslint no-console:0 */
+  window && window.console && console.error && console.error(ex)
+}
