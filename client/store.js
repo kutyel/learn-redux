6428a8ae46355b6b36117d11ab60c4e3c91b1e08
kutyel <flaviocorpa@gmail.com>
2017-05-30 13:39:35 +0200

    Add reducers hot reloading

--- client/store.js
+++ client/store.js
@@ -10,6 +10,12 @@ import comments from './data/comments'
 const defaultState = { posts, comments }
 
 const store = createStore(reducer, defaultState)
+
 export const history = syncHistoryWithStore(browserHistory, store)
+
+if (module.hot) {
+  module.hot.accept('./reducers/', () =>
+    store.replaceReducer(require('./reducers/index').default))
+}
 
 export default store
