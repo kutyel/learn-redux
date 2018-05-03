ffc73df4171bffb022b9d774db2d4b142f3ae722
kutyel <flaviocorpa@gmail.com>
2018-05-03 19:42:14 +0200

    Move API access_token to .env

--- client/components/Single.js
+++ client/components/Single.js
@@ -3,11 +3,9 @@ import React from 'react'
 import Photo from './Photo'
 import Comments from './Comments'
 
-import store from '../store'
-
 const Single = props => {
   const { postId } = props.params
-  const index = props.posts.findIndex(p => p.id === postId)
+  const index = props.posts.findIndex(({ id }) => id === postId)
   const post = props.posts[index]
   const comments = props.comments[postId] || []
   props.loadComments(postId)
